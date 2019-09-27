eeb959ffaaea19558735c20209ca60603d2062e4
LUIS J Alvarez Diaz <ldiaz244@gmail.com>
2019-09-27 02:19:58 +0000

    Done.

---
+++ lib/student.rb
@@ -0,0 +1,9 @@
+class Student
+  attr_accessor = :hello
+  def hello
+    puts "Hey there! I'm so excited to learn stuff."
+    @student.new = "Pick me!"
+    def "Pick me!'
+    puts Pick me!
+  end
+end
